4309b3f48a2534bfd31f573b0331314adeec859d
CCyban <47162481+CCyban@users.noreply.github.com>
2021-09-17 14:16:43 +0100

    Initial setup

---
+++ kanban-web-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <KanbanList msg="Welcome to Your Vue.js App" :kanbanBoards="kanbanBoards"/>
+</template>
+
+<script>
+import KanbanList from './components/KanbanList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    KanbanList
+  },
+  data() {
+    return {
+      kanbanBoards: ['summat', 'summat2', 'summat3'],
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ kanban-web-app/src/components/KanbanList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>Kanban board list</h1>
+    <p>
+      Made using Vue.JS 3 and other stuff
+    </p>
+
+    <h3>Kanban boards</h3>
+    <ul>
+      <li v-for="value in kanbanBoards" v-bind:key="value">
+        {{ value }}
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    kanbanBoards: Array,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
