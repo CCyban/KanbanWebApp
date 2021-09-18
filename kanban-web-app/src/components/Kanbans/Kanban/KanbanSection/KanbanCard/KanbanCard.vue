81f9771548941015035666738cc0a2cb6d321398
CCyban <47162481+CCyban@users.noreply.github.com>
2021-09-18 20:25:38 +0100

    Made b-modal appear when you open a kanban card (and disappear appropriately)

--- kanban-web-app/src/components/Kanbans/Kanban/KanbanSection/KanbanCard/KanbanCard.vue
+++ kanban-web-app/src/components/Kanbans/Kanban/KanbanSection/KanbanCard/KanbanCard.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
     <b-overlay
         v-b-hover="hoverHandle"
         :show="isCardHovered"
@@ -16,24 +17,34 @@
             <b-button
                 variant="outline-success"
                 size="sm"
-                @click="show = false">
+                @click="showModal = true">
                     Open
                 <b-icon icon="pencil-square" />
             </b-button>
             </div>
         </template>
     </b-overlay>
+    <b-modal v-model="showModal" hide-footer title="Using Component Methods" hide-header-close>
+        <kanban-card-edit @toggleModal="toggleModal()"/>
+    </b-modal>
+</div>
 </template>
 
 <script>
+import KanbanCardEdit from './KanbanCardEdit.vue'
+
 export default {
     name: 'KanbanCard',
     props: {
         content: String,
     },
+    components: {
+        KanbanCardEdit,
+    },
     data() {
         return {
             isCardHovered: false,
+            showModal: false,
         }
     },
     methods: {
@@ -44,6 +55,9 @@ export default {
             else {
                 this.isCardHovered = false;
             }
+        },
+        toggleModal() {
+            this.showModal = !this.showModal;
         }
     }
 }
---
+++ kanban-web-app/src/components/Kanbans/Kanban/KanbanSection/KanbanCard/KanbanCardEdit.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="d-block text-center">
+            <h3>Editing thingy</h3>
+        </div>
+        <b-button class="mt-3" variant="outline-danger" block @click="hideModal">Close Me</b-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'KanbanCardEdit',
+    methods: {
+        hideModal() {
+            this.$emit('toggleModal');
+      }
+    }
+}
+</script>
